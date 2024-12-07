a29ee0d97b05870b4f0a110b9bd852b7996dccab
Muhammad Asif <142832379+Muhammadasif646@users.noreply.github.com>
2024-12-07 02:34:57 -0800

    Add files via upload

---
+++ file.js
@@ -0,0 +1,27 @@
+
+
+let i=1
+
+
+function inc(){
+
+    document.getElementsByClassName("h")[0].innerHTML=i++
+
+
+}
+
+function dic(){
+
+    document.getElementsByClassName("h")[0].innerHTML=i--
+
+
+}
+
+function res(){
+
+
+
+    document.getElementsByClassName("h")[0].innerHTML=i=0
+
+
+}
